fc2500e4654acbc344de426eeef5e4123276ade6
SoundhararajanL <133208000+SoundhararajanL@users.noreply.github.com>
2023-11-22 11:17:29 +0530

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -506,7 +506,7 @@ class GripperList extends Component {
 
           {/* Checkbox inputs for Manufacture Name */}
           <div className="checkbox-section">
-            <label>Manufacture Name:</label>
+            <label>Manufacture  area:</label>
             {filterOptions.manufactureNames.slice(0, this.state.displayManufactureNames).map((option, index) => (
               <div key={index} className="checkbox-item">
                 <input
