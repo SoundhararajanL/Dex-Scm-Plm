d803d3ec32cf6c57a90c7cd9e9d4b960002c9e3f
SoundhararajanL <sowndhar00001@gmail.com>
2023-12-21 10:21:31 +0530

    new one

--- jsonbackend/server.js
+++ jsonbackend/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs/promises');
 const cors = require('cors');  
 const app = express();
-const port = 3001; 
+const port = 3000; 
 const corsOptions = {
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
